c7a89f5f2fc770dd4bf0cc8113e51661fd7dc29c
mkpanfil <milosz.panfil@fuw.edu.pl>
2026-04-07 15:12:33 +0200

    move free_energy from TBAState to thermodynamics module

--- rapidity/thermodynamics.py
+++ rapidity/thermodynamics.py
@@ -14,7 +14,46 @@ quantities from a :class:`~rapidity.tba.TBAState`:
 import numpy as np
 from scipy.optimize import brentq
 from rapidity.core import Grid1D, Field
-from rapidity.tba import TBAState, Model
+from rapidity.tba import TBAState, StringTBAState
+from rapidity.models import Model, StringModel
+
+
+def free_energy(state: TBAState) -> float:
+    """Compute the free energy density.
+
+    For a single particle species, the free energy density is given by
+    .. math::
+
+        f = -\\int \\frac{d\\theta}{2\\pi}
+        \\log(1 + e^{-\\epsilon(\\theta)})
+
+    For string models, the free energy density is given by
+    .. math::
+
+        f = -\\sum_n \\int d\\theta\\,
+        a_n(\\theta) \\log(1 + e^{-\\epsilon_n(\\theta)})
+
+    Returns
+    -------
+    float
+        The free energy density.
+    """
+    # epsilon = Field(
+    #     np.log((1 - self.filling.values) / self.filling.values), [self.grid]
+    # )
+    # the code is not protected against dividing by 0.
+    if isinstance(state.model, Model):
+        epsilon = state.filling.apply(lambda n: np.log((1 - n) / n))
+        log_term = Field(np.log(1 + np.exp(-epsilon.values)), [state.grid])
+        return -(log_term / (2 * np.pi)).integrate().values
+    elif isinstance(state.model, StringModel):
+        total = 0.0
+        for n, filling in enumerate(state.filling):
+            a_n = state.model.bare_state_density(n + 1, state.grid)
+            epsilon = filling.apply(lambda x: np.log((1 - x) / x))
+            log_term = epsilon.apply(lambda x: np.log(1 + np.exp(-x)))
+            total += -(a_n * log_term).integrate().values
+        return total
 
 
 def find_mu(
@@ -92,3 +131,25 @@ def sound_velocity(state: TBAState) -> float:
         The sound velocity.
     """
     ...
+
+
+def magnetization(state: StringTBAState) -> float:
+    """Compute the total magnetization per site.
+
+    .. math::
+
+        m = S - \\sum_n n \\int d\\theta\\, \\rho_p^n(\\theta)
+
+    Parameters
+    ----------
+    state : StringTBAState
+        The thermodynamic state.
+
+    Returns
+    -------
+    float
+        The magnetization per site.
+    """
+    rho_p = state.rho_p()
+    total = sum((n + 1) * rho_p[n].integrate().values for n in range(state.model.n_max))
+    return state.model.S - total
